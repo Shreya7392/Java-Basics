d1f41ec4e761ce0fed5d5362ba1a40c3f9b779cd
Shreya7392 <shreyaghosh1213@gmail.com>
2026-08-27 10:55:55 +0530

    Update the changes

--- loop/CountNumbers.java
+++ loop/CountNumbers.java
@@ -1,5 +1,21 @@
 package loop;
 
+import java.util.*;
 public class CountNumbers {
+    public static void main(String [] args ){
+        System.out.println( "Enter Number :");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        while(n!=0){
+            n/=10;
+            count++;
+         }
+         System.out.println(count);
+
+
+    }
+
+    
     
 }
