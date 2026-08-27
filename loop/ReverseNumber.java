a43a87ac97ed0ecfbe06177147a240ff75141f62
Shreya7392 <shreyaghosh1213@gmail.com>
2026-08-27 11:15:47 +0530

    Update the changes

---
+++ loop/ReverseNumber.java
@@ -0,0 +1,17 @@
+package loop;
+import java.util.*;
+public class ReverseNumber {
+    public static void main(String[] args) {
+        System.out.println("Enter Number:");
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int r = 0;
+        while(n!=0){
+            r*=10;
+            r+=(n%10);
+            n/=10;
+        }
+        System.out.println(r);
+    }
+    
+}
