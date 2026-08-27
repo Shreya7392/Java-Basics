3d71c339808628a7bd4e81d76446f08288b4d5bf
Shreya7392 <shreyaghosh1213@gmail.com>
2026-08-27 22:40:17 +0530

    Update the changes

---
+++ Methods/MaxNumberInput.java
@@ -0,0 +1,15 @@
+package Methods;
+import java.util.*;
+public class MaxNumberInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a:");
+        int a = sc.nextInt();
+         System.out.println("Enter b:");
+        int b = sc.nextInt();
+         System.out.println("Enter c:");
+        int c = sc.nextInt();
+        System.out.println("The max number is :" + " " + Math.max(Math.max(a,b),c));
+        
+    }
+}
