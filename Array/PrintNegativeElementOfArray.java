387c184b0d6015a538d29853c2efdff8dfd46678
Shreya7392 <shreyaghosh1213@gmail.com>
2026-08-28 18:29:08 +0530

    Update changes

---
+++ Array/PrintNegativeElementOfArray.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.*;
+
+public class PrintNegativeElementOfArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Array Size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter Array Elements: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Negative Elements:");
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
