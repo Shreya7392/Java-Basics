60dff5ad61c96952918c07bd13a1692bc61c275e
Shreya Ghosh <shreyaghosh1213@gmail.com>
2026-08-30 15:01:30 +0530

    add new changes

---
+++ Array/TwoSum.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class TwoSum {
+    public static void main(String[] args){
+        System.out.println("Two sum");
+    }
+    
+   public static boolean twoSum(int arr[], int target) {
+        int n =arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]+arr[j]==target);
+                return true;
+                
+            }       
+        }
+         return  false;
+    }
+}
+
